4a861e0765f5ef9fbe136b478d75c894dde5c26a
Ram Kumar Bhandari <rambhandari@lftechnology.com>
2020-01-17 21:35:04 +0545

    Add secondary button component and styles

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Button.css';
 
-export const Button = ({ onClick, children }) => {
+export const PrimaryButton = ({ onClick, children }) => {
   return (
     <button type="button" className="Button PrimaryButton" onClick={onClick}>
       {children}
     </button>
   );
 };
+
+export const SecondaryButton = ({ onClick, children }) => {
+  return (
+    <button type="button" className="Button SecondaryButton" onClick={onClick}>
+      {children}
+    </button>
+  )
+}
