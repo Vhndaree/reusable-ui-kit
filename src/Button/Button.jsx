80a0bb80b24aa62dd0a641f37a4d80e6b695d3e6
Ram Kumar Bhandari <rambhandari@lftechnology.com>
2020-01-17 17:47:21 +0545

    Add onclick event on button component.

--- src/Button/Button.jsx
+++ src/Button/Button.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Button.css';
 
-export const Button = () => {
+export const Button = props => {
+  const { onClick } = props;
   return (
-    <button type="button" className="Button PrimaryButton">
+    <button type="button" className="Button PrimaryButton" onClick={onClick}>
       click
     </button>
   );
